26873e30397b29be353b96367cc7a7420f864173
jhcho <finetoday@daum.net>
2023-03-01 14:19:24 +0900

    Initial commit

---
+++ hello_world-master/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello Code Factory !!',
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
